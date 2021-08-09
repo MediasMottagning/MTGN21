cf17183d65383e3da3750f5528da8705bac3123c
astudic <58708697+astudic@users.noreply.github.com>
2021-08-09 14:36:54 +0200

    update footer adding sponsors

--- frontend/src/Footer/Footer.jsx
+++ frontend/src/Footer/Footer.jsx
@@ -4,19 +4,19 @@ import RR from '../assets/easter-eggs/RR.png'
 //import eric from '/assets/companies/ericsson.png'
 
 class Footer extends Component {
-  //Saknas Unionen och SVT 
-  //Ericsson och sveriges ingenjörer är loggor från förra året!
 
   sponsors = [
   {url:"https://www.sverigesingenjorer.se/bli-medlem/", img:require('../assets/companies/sverigesingenjörer.jpg')},
   {url:"https://mpyadigital.com/", img:require('../assets/companies/mpya_digital.png')},
-  {url:"https://kollin.io/", img:require('../assets/companies/kollin.png')},
+  {url:"https://www.cgi.com/se/sv", img:require('../assets/companies/cgi.png')},
+  {url:"https://www.columbiaroad.com/", img:require('../assets/companies/columbiaRoad.svg')},
 
   {url:"https://schibsted.com/", img:require('../assets/companies/Schibsted-logo-medium.svg')},
 
+  {url:"https://www.redbull.com/se-en/", img:require('../assets/companies/RedBull.png')},
   {url:"https://studybuddy.se/", img:require('../assets/companies/Studybuddy.png')},
-  {url:"https://www.ericsson.com/en", img:require('../assets/companies//ericsson.png')}]
- // {url:"https://www.svt.se/", img:require('../assets/companies//SVT_Logotyp_RGB.png')}]
+  {url:"https://www.ericsson.com/en", img:require('../assets/companies//ericsson.png')},
+  {url:"https://www.svt.se/", img:require('../assets/companies//SVT_Logotyp_RGB.png')}]
 
   render() {
     return (
@@ -24,7 +24,7 @@ class Footer extends Component {
         {this.sponsors.map((sponsor, i) => (
           <a key={i} className='footer-linck' href={sponsor.url}><img src={sponsor.img} height="40px" alt="sponsor"/></a>
         ))}
-       <a className='footer-linck' href="https://www.youtube.com/embed/oHg5SJYRHA0?autoplay=1"><img src={RR} height="40px" alt="sponsor"/></a>
+       <a className='footer-linck' href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"><img src={RR} height="40px" alt="sponsor"/></a>
     </div>
     );
   }
